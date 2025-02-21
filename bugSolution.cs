988f0abfff6fa874fc72d489d009a4ddc950130b
BugHunter-X <askzenai@gmail.com>
2025-02-21 14:48:05 +0100

    Add bugSolution.cs file

---
+++ bugSolution.cs
@@ -0,0 +1,16 @@
+public class ExampleClass
+{
+    private int _myField;
+
+    public int MyProperty
+    {
+        get { return _myField; }
+        set { _myField = value; }
+    }
+
+    public void MyMethod()
+    {
+        // Accessing the field using the property
+        MyProperty = 10; //Correct way
+    }
+}
